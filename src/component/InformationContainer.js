e608d3feab99e196b17257e1d9d0e6b2ae09cbb0
Bilal Khan <bilal.khan@slickwhiz.com>
2022-12-07 01:24:19 +0500

    info and cv updated

--- src/component/InformationContainer.js
+++ src/component/InformationContainer.js
@@ -7,7 +7,7 @@ export default function InformationContainer() {
         <div>
           <p className='headingName'>Muhammad Bilal Khan</p>
           <p className='headingProfession'>Front-end/JavaScript Developer</p>
-          <p className='descriptionText'>Hi, I am on my prime time of Career in which I have a long way to chase my dreams.<br/> I develop what you desire.</p>
+          <p className='descriptionText'>Hi, I have almost 2 years of professional development experiance.<br/> I develop what you desire.</p>
           <div className='informationButtonsContainer'>
             <span><div className='sideHorizontalLine'></div><p><a href='./files/MuhammadBilalKhanCV.pdf' download >Download CV</a></p></span>
             <span><div className='sideHorizontalLine'></div><p>Experience</p></span>
--- src/component/NavContent.js
+++ src/component/NavContent.js
@@ -5,7 +5,7 @@ import { faHome } from '@fortawesome/free-solid-svg-icons';
 export default function NavContent() {
     return (
         <div>
-            <div className="x-button-container" id='defaultMenuStyle'>
+          <div className="x-button-container" id='defaultMenuStyle'>
             <div className="bar1"></div>
             <div className="bar2"></div>
             <div className="bar3"></div>
